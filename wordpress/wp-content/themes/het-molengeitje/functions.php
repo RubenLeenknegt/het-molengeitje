178d7f49d2ef4f98be8b935c35645cfdf7a6d82b
RubenLeenknegt <r.leenknegt@pm.me>
2026-04-25 13:45:04 +0200

    File write permissions tweak

--- wordpress/wp-content/themes/het-molengeitje/functions.php
+++ wordpress/wp-content/themes/het-molengeitje/functions.php
@@ -3,3 +3,12 @@
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('theme-style', get_template_directory_uri() . '/assets/css/dist/app.css', [], '1.0');
 });
+
+add_filter('acf/settings/save_json', function () {
+    return get_stylesheet_directory() . '/acf-json';
+});
+
+add_filter('acf/settings/load_json', function ($paths) {
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+    return $paths;
+});
