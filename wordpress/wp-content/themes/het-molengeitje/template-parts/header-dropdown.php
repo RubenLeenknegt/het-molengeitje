d04a19273dda96e28efa35fc7bc3e0e59c129c82
RubenLeenknegt <r.leenknegt@pm.me>
2026-04-26 18:06:30 +0200

    Feat: Navmenu dropdown

--- wordpress/wp-content/themes/het-molengeitje/template-parts/header-dropdown.php
+++ wordpress/wp-content/themes/het-molengeitje/template-parts/header-dropdown.php
@@ -4,11 +4,11 @@ $primary_menu_exists = has_nav_menu('primary');
 
 <div
     id="site-dropdown"
-    class="dropdown-panel absolute left-0 top-full w-full border-t border-black/10 bg-white"
+    class="dropdown-panel absolute left-0 top-full w-full bg-white"
     data-dropdown-panel
 >
     <div class="mx-auto grid w-full max-w-[1440px] gap-8 px-6 py-8 sm:px-10 md:grid-cols-[minmax(0,1fr)_320px] md:px-14 lg:px-20">
-        <div class="pt-1">
+        <div class="pt-8">
             <?php if ($primary_menu_exists) : ?>
                 <?php
                 wp_nav_menu([
