4d6bdf6a34853d66d1bf271baa5ae7c882558992
RubenLeenknegt <r.leenknegt@pm.me>
2026-04-30 14:02:42 +0200

    Feat: feature split mobile pass

--- wordpress/wp-content/themes/het-molengeitje/template-parts/feature-split.php
+++ wordpress/wp-content/themes/het-molengeitje/template-parts/feature-split.php
@@ -13,11 +13,11 @@ $image_url     = $image['url'] ?? '';
 $image_alt     = $image['alt'] ?? '';
 ?>
 
-<section class="bg-[color:var(--color-secondary-bg)] px-6 py-20 sm:px-10 md:px-14 lg:px-20 rounded-4xl">
-    <div class="mx-auto flex w-full max-w-[1440px] flex-col gap-4 md:flex-row md:items-stretch md:gap-5">
-        <div class="order-1 w-full md:order-2 md:basis-2/5">
+<section class="bg-[color:var(--color-secondary-bg)] px-6 py-16 sm:px-10 sm:py-18 md:px-14 md:py-20 lg:px-20 lg:py-20 rounded-4xl">
+    <div class="mx-auto flex w-full max-w-[1440px] flex-col gap-6 lg:flex-row lg:items-stretch lg:gap-5">
+        <div class="order-1 w-full lg:order-2 lg:basis-5/12">
             <?php if ($image_url) : ?>
-                <div class="h-full w-full overflow-hidden" style="border-radius: 120px 24px 24px 120px;">
+                <div class="h-full w-full overflow-hidden rounded-3xl md:rounded-3xl lg:rounded-[120px_24px_24px_120px]">
                     <img
                         src="<?php echo esc_url($image_url); ?>"
                         alt="<?php echo esc_attr($image_alt); ?>"
@@ -28,14 +28,14 @@ $image_alt     = $image['alt'] ?? '';
             <?php endif; ?>
         </div>
 
-        <div class="order-2 w-full md:order-1 md:basis-3/5">
-            <div class="h-full pr-70 ps-25 py-20 bg-white p-12 text-black" style="border-radius: 24px 120px 120px 24px;">
+        <div class="order-2 w-full lg:order-1 lg:basis-7/12">
+            <div class="h-full bg-white p-8 sm:p-10 md:py-14 md:ps-14 md:pe-20 lg:py-20 lg:px-25 2xl:pr-70 text-black rounded-3xl md:rounded-3xl lg:rounded-[24px_120px_120px_24px]">
                 <?php if ($title) : ?>
-                    <h2 class="mb-10 pr-10 text-black [&_span]:text-(--color-secondary)"><?php echo $title ?></h2>
+                    <h2 class="mb-6 sm:mb-8 md:mb-10 pr-0 sm:pr-6 md:pr-4 text-black [&_span]:text-(--color-secondary)"><?php echo $title ?></h2>
                 <?php endif; ?>
 
                 <?php if ($description) : ?>
-                    <p class="mb-10 text-black"><?php echo esc_html($description); ?></p>
+                    <p class="mb-6 sm:mb-8 md:mb-10 text-black"><?php echo esc_html($description); ?></p>
                 <?php endif; ?>
 
                 <?php if ($button_text && $button_url) : ?>
