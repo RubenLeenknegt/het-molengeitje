2bf5e7c30c83ed9196e767a9d439e126c775c1d4
RubenLeenknegt <r.leenknegt@pm.me>
2026-05-01 19:52:56 +0200

    Feat: showcase block css tweaks

--- wordpress/wp-content/themes/het-molengeitje/template-parts/showcase-section.php
+++ wordpress/wp-content/themes/het-molengeitje/template-parts/showcase-section.php
@@ -26,18 +26,18 @@ $bullets          = array_filter([
 
 <section class="bg-white px-6 py-16 sm:px-10 sm:py-18 md:px-14 md:py-20 lg:py-5 lg:px-20 rounded-4xl">
     <div
-        class="mx-auto flex w-full max-w-[1440px] items-center justify-center overflow-hidden rounded-4xl bg-cover bg-center"
+        class="mx-auto flex w-full max-w-[1440px] overflow-hidden rounded-4xl bg-cover bg-center"
         <?php if ($bg_url) : ?>
             style="background-image: url('<?php echo esc_url($bg_url); ?>');"
             role="img"
             aria-label="<?php echo esc_attr($bg_alt); ?>"
         <?php endif; ?>
     >
-        <div class="w-full bg-black/40 px-6 py-10 sm:px-10 sm:py-12 md:px-14 md:py-16 lg:px-20 lg:py-20">
-            <div class="mx-auto grid w-full max-w-5xl grid-cols-1 gap-8 text-left text-white lg:grid-cols-[3fr_2fr] lg:gap-40">
+        <div class="w-full bg-black/50 px-6 py-10 sm:px-10 sm:py-12 md:px-14 md:py-16 lg:px-30 lg:py-20">
+            <div class="grid w-full max-w-6xl grid-cols-1 gap-8 text-left text-white lg:grid-cols-[3fr_2fr] lg:gap-24">
                 <div class="flex flex-col">
                     <?php if ($title) : ?>
-                        <h2 class="mb-6 sm:mb-8 md:mb-10 text-white [&_span]:text-(--color-secondary)"><?php echo $title; ?></h2>
+                        <h2 class="mb-6 sm:mb-8 md:mb-15 text-white [&_span]:text-(--color-secondary)"><?php echo $title; ?></h2>
                     <?php endif; ?>
 
                     <?php if ($description) : ?>
@@ -65,13 +65,13 @@ $bullets          = array_filter([
 
                 <div class="flex flex-col">
                     <?php if (!empty($bullets)) : ?>
-                        <ul class="mt-41 flex w-full flex-col">
+                        <ul class="mt-46 flex w-full flex-col">
                             <?php foreach ($bullets as $bullet) : ?>
                                 <li class="flex items-start gap-3">
                                     <span class="mt-0.5 inline-flex h-6 w-6 flex-none items-center justify-center text-white">
                                         <?php echo file_get_contents(get_template_directory() . '/assets/media/svg/checkmark.svg'); ?>
                                     </span>
-                                    <span class="text-white/90"><?php echo esc_html($bullet); ?></span>
+                                    <span class="text-nowrap mb-5 text-white/90"><?php echo esc_html($bullet); ?></span>
                                 </li>
                             <?php endforeach; ?>
                         </ul>
